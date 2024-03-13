e9ef14c0c63a5f066297f715233247fa34e42627
weiwei0304 <weiwendy0430@gmail.com>
2024-03-13 18:02:20 +0800

    1:31:31

---
+++ src/components/Benefits.jsx
@@ -0,0 +1,26 @@
+import { benefits } from "../constants";
+import Section from "./Section";
+import Heading from "./Heading";
+const Benefits = () => {
+  return (
+    <Section id="features">
+      <div className="container relative z-2">
+        <Heading
+          className="md:max-w-md lg:max-w-2xl"
+          title="Chat Smarter, Not Harder with Brainwave"
+        />
+        <div className="flex flex-wrap gap-10 mb-10">
+          {benefits.map((item) => (
+            <div key={item.id}>
+              <div>
+                <h5>{item.title}</h5>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </Section>
+  );
+};
+
+export default Benefits;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,6 +5,9 @@ import { curve, robot, heroBackground } from "../assets";
 import { BackgroundCircles, BottomLine, Gradient } from "./design/Hero";
 import { heroIcons } from "../constants";
 import { ScrollParallax } from "react-just-parallax";
+import Generating from "./Generating";
+import Notification from "./Notification";
+import CompanyLogos from "./CompanyLogos";
 const Hero = () => {
   const parallaxRef = useRef(null);
 
@@ -19,9 +22,9 @@ const Hero = () => {
       <div className="container relative" ref={parallaxRef}>
         <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[4rem] md:mb-20 lg:mb-[6rem]">
           <h1 className="h1 mb-6">
-            Explore the Possibilities of AI Chatting with Brainwave
+            Explore the Possibilities of&nbsp;AI&nbsp;Chatting with Brainwave
             <span className="inline-block relative">
-              Brainwave{" "}
+              {" "}
               <img
                 src={curve}
                 className="absolute top-full left-0 w-full xl:-mt-2"
@@ -51,8 +54,29 @@ const Hero = () => {
                   height={490}
                   alt="AI"
                 />
+
+                <Generating className="absolute left-4 right-4 bottom-5 md:left-1/2 md:right-auto md:bottom-8 md:w-[31rem] md:-translate-x-1/2" />
+
+                <ScrollParallax isAbsolutelyPositioned>
+                  <ul className="hidden absolute -left-[5.5rem] bottom-[7.5rem] px-1 py-1 bg-n-9/40 backdrop-blur border border-n-1/10 rounded-2xl xl:flex">
+                    {heroIcons.map((icon, index) => (
+                      <li className="p-5" key={index}>
+                        <img src={icon} width={24} height={25} alt={icon} />
+                      </li>
+                    ))}
+                  </ul>
+                </ScrollParallax>
+
+                <ScrollParallax isAbsolutelyPositioned>
+                  <Notification
+                    className="hidden absolute -right-[5.5rem] bottom-[11rem] w-[18rem] xl:flex"
+                    title="Code generation"
+                  />
+                </ScrollParallax>
               </div>
             </div>
+
+            <Gradient />
           </div>
           <div className="absolute -top-[54%] left-1/2 w-[234%] -translate-x-1/2 md:-top-[46%] md:w-[138%] lg:-top-[104%]">
             <img
@@ -63,8 +87,13 @@ const Hero = () => {
               alt="hero"
             />
           </div>
+
+          <BackgroundCircles />
         </div>
+
+        <CompanyLogos className="hidden relative z-10 mt-20 lg:block" />
       </div>
+      <BottomLine />
     </Section>
   );
 };
