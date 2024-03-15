7993dbb9e047a0b1fe5e4ab33ee5bea8d4eb7e4d
weiwei0304 <weiwendy0430@gmail.com>
2024-03-15 14:30:11 +0800

    1:40:59

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -1,9 +1,11 @@
-import { benefits } from '../constants';
-import Section from './Section';
-import Heading from './Heading';
-import Arrow from '../assets/svg/Arrow';
+import { benefits } from "../constants";
+import Section from "./Section";
+import Heading from "./Heading";
+import Arrow from "../assets/svg/Arrow";
 
-import { GradientLight } from './design/Benefits';
+import { GradientLight } from "./design/Benefits";
+
+import ClipPath from "../assets/svg/ClipPath";
 
 const Benefits = () => {
   return (
@@ -23,7 +25,7 @@ const Benefits = () => {
               key={item.id}
             >
               <div className="relative z-2 flex flex-col min-h-[22rem] p-[2.4rem] pointer-events-none">
-                <h5 className="h2 mb-5">{item.title}</h5>
+                <h5 className="h5 mb-5">{item.title}</h5>
                 <p className="body-2 mb-6 text-n-3">{item.text}</p>
                 <div className="flex items-center mt-auto">
                   <img
@@ -43,19 +45,22 @@ const Benefits = () => {
 
               <div
                 className="absolute inset-0.5 bg-n-8"
-                style={{ clipPath: 'url(#benefits' }}
+                style={{ clipPath: "url(#benefits" }}
               >
-                <div>
+                <div className="absolute inset-0 opacity-0 transition-opacity hover:opacity-10">
                   {item.imageUrl && (
                     <img
                       src={item.imageUrl}
                       width={380}
                       height={362}
                       alt={item.title}
+                      className="w-full h-full object-cover"
                     />
                   )}
                 </div>
               </div>
+
+              <ClipPath />
             </div>
           ))}
         </div>
